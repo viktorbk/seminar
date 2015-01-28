cf5668bb6c8e3bc3bc9d75e67bf03d13f0592eeb
Tor Anders Johansen <mail@toranders.com>
2015-01-28 21:56:33 +0100

    Added KursApiTests

---
+++ Seminar.Business.Tests/KursApiTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Seminar.Business.Api;
+
+namespace Seminar.Business.Tests
+{
+    [TestFixture]
+    public class KursApiTests
+    {
+        [Test]
+        public void KursApi_GetAllKurs_RetrieveAllKurs_Success()
+        {  
+            // Arrange
+            IKursApi kursApi = new KursApi();
+
+            // Act
+
+            // Assert
+        }
+    }
+
+
+}
